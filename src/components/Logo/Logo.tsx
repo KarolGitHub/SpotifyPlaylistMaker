accda8a07f7ad7d65e703e7528c5220a3411dc85
Karol <karolgardyjas@gmail.com>
2020-11-08 14:19:35 +0100

    Add link to home page in App Logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import Logo from "../../assets/logo.png";
 import classes from "./Logo.module.scss";
 
 const logo = () => (
   <div className={classes.Logo}>
-    <img src={Logo} alt="MyPlaylist" />
+    <NavLink to="/" exact>
+      <img src={Logo} alt="MyPlaylist" />
+    </NavLink>
   </div>
 );
 
